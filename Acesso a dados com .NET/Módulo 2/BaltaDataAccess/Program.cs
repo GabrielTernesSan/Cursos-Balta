56cab852a1fb9a25d92f3bf9b6551d500089db6c
Gabriel <santos.tgabriel@hotmail.com>
2022-08-10 18:15:47 -0300

    Módulo 2 - UPDATE

--- Acesso a dados com .NET/Módulo 2/BaltaDataAccess/Program.cs
+++ Acesso a dados com .NET/Módulo 2/BaltaDataAccess/Program.cs
@@ -12,6 +12,24 @@ namespace BaltaDataAccess
             const string connectionString 
                 = "Server=localhost,1433;Database=balta;User ID=sa;Password=G@abriel613390;TrustServerCertificate=True;";
 
+            using (var connection = new SqlConnection(connectionString))
+            {
+                ListCategories(connection);
+                // CreateCategory(connection);
+                UpdateCategory(connection);
+                ListCategories(connection);
+            }
+        }
+        static void ListCategories(SqlConnection connection)
+        {
+            var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category] ORDER BY [Title]");
+            foreach (var category in categories)
+            {
+                Console.WriteLine($"{category.Id} - {category.Title}");
+            }
+        }
+        static void CreateCategory(SqlConnection connection)
+        {
             var categoria = new Category();
             categoria.Id = Guid.NewGuid();
             categoria.Title = "Amazon AWS";
@@ -20,7 +38,7 @@ namespace BaltaDataAccess
             categoria.Order = 8;
             categoria.Description = "Categoria destinada a serviços do AWS";
             categoria.Featured = false;
-            
+
             // SQL Injection
             // Para evitar esse tipo de ataque,
             // NUNCA usar a concatenação de strings ($)
@@ -38,28 +56,33 @@ namespace BaltaDataAccess
                                 @Description,
                                 @Featured)";
 
-            using (var connection = new SqlConnection(connectionString))
+            // Retorna o número de linhas afetadas
+            var rows = connection.Execute(insertSql, new
             {
-                // Retorna o número de linhas afetadas
-                var rows = connection.Execute(insertSql, new
-                {
-                    categoria.Id,
-                    categoria.Title,
-                    categoria.Url,
-                    categoria.Summary,
-                    categoria.Order,
-                    categoria.Description,
-                    categoria.Featured
-                });
+                categoria.Id,
+                categoria.Title,
+                categoria.Url,
+                categoria.Summary,
+                categoria.Order,
+                categoria.Description,
+                categoria.Featured
+            });
 
-                Console.WriteLine($"{rows} - linhas inseridas");
+            Console.WriteLine($"{rows} - linhas inseridas");
+        }
+        static void UpdateCategory(SqlConnection connection)
+        {
+            var updateQuery = "UPDATE [Category] SET [Title]=@Title WHERE [Id]=@Id";
 
-                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category] ORDER BY [Title]");
-                foreach (var category in categories)
-                {
-                    Console.WriteLine($"{category.Id} - {category.Title}");
-                }
-            }
+            var rows = connection.Execute(updateQuery, new
+            {
+                // Também funciona
+                // Id = "AF3407AA-11AE-4621-A2EF-2028B85507C4",
+                Id = new Guid("AF3407AA-11AE-4621-A2EF-2028B85507C4"),
+                Title = "Frontend 2021"
+            });
+
+            Console.WriteLine($"{rows} - Registros atualizados;");
         }
     }
 }
