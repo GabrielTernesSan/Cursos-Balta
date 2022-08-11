9f0dd269190cc800b47c4cbab77e766c3fcbd58d
Gabriel <santos.tgabriel@hotmail.com>
2022-08-11 10:54:53 -0300

    QueryMultiple

--- Acesso a dados com .NET/Módulo 3/BaltaDataAccess/Program.cs
+++ Acesso a dados com .NET/Módulo 3/BaltaDataAccess/Program.cs
@@ -26,7 +26,8 @@ namespace BaltaDataAccess
                 // ExecuteScalar(connection);
                 // ReadView(connection);
                 // OneToOne(connection);
-                OneToMany(connection);
+                // OneToMany(connection);
+                QueryMultiple(connection);
             }
         }
         static void ListCategories(SqlConnection connection)
@@ -324,5 +325,25 @@ namespace BaltaDataAccess
             }
         }
 
+        static void QueryMultiple(SqlConnection connection)
+        {
+            var query = "SELECT * FROM [Category]; SELECT * FROM [Course]";
+
+            using(var multi = connection.QueryMultiple(query))
+            {
+                var categories = multi.Read<Category>();
+                var courses = multi.Read<Course>();
+
+                foreach(var item in categories)
+                {
+                    Console.WriteLine(item.Title);
+                }
+
+                foreach (var item in courses)
+                {
+                    Console.WriteLine(item.Title);
+                }
+            }
+        }
     }
 }
