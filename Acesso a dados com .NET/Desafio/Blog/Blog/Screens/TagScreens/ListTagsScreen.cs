7afcbbfe168b3ff0ff5c0e47fe227c5776663406
Gabriel <santos.tgabriel@hotmail.com>
2022-08-13 21:42:52 -0300

    Compartilhando a conexão

--- Acesso a dados com .NET/Desafio/Blog/Blog/Screens/TagScreens/ListTagsScreen.cs
+++ Acesso a dados com .NET/Desafio/Blog/Blog/Screens/TagScreens/ListTagsScreen.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Blog.Models;
+using Blog.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +12,20 @@ namespace Blog.Screens.TagScreens
     {
         public static void Load()
         {
+            Console.Clear();
+            Console.WriteLine("Lista de tags");
+            Console.WriteLine("--------------");
+            List();
+            Console.ReadKey();
+        }
 
+        private static void List()
+        {
+            var repository = new Repository<Tag>(Database.Connection);
+
+            var tags = repository.Get();
+            foreach(var item in tags)
+                Console.WriteLine($"{item.Id} - {item.Name} ({item.Slug})");
         }
     }
 }
