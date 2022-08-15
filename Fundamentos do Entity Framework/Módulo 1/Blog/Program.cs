08d3f70a60a4dbbc392acda175746255675c3ffb
Gabriel <santos.tgabriel@hotmail.com>
2022-08-15 08:56:33 -0300

    Configurando o DataContext

--- Fundamentos do Entity Framework/Módulo 1/Blog/Program.cs
+++ Fundamentos do Entity Framework/Módulo 1/Blog/Program.cs
@@ -1,11 +1,16 @@
 ﻿
+using Blog.Data;
+
 namespace Blog
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            using(var context = new BlogDataContext())
+            {
+
+            }
         }
     }
 }
