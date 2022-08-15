3203bdf76ea8c2bd70895c1320d8ccacf4f0bf66
Gabriel <santos.tgabriel@hotmail.com>
2022-08-15 13:09:44 -0300

    ToList

--- Fundamentos do Entity Framework/Módulo 1/Blog/Program.cs
+++ Fundamentos do Entity Framework/Módulo 1/Blog/Program.cs
@@ -23,10 +23,33 @@ namespace Blog
                 //context.Update(tag);
                 //context.SaveChanges();
 
-                var tag = context.Tags.FirstOrDefault(x => x.Id == 4);
+                //var tag = context.Tags.FirstOrDefault(x => x.Id == 4);
 
-                context.Remove(tag);
-                context.SaveChanges();
+                //context.Remove(tag);
+                //context.SaveChanges();
+
+                // var tags = context.Tags;
+                // Não executa a Query
+                // Só seria executada quando chamada no foreach
+
+                
+                var tags1 = context // Chegaria no contexto
+                    .Tags         // Chegaria nas tags
+                    .ToList()    // Executaria um SELECT em todas as tags
+                    .Where(x => x.Name.Contains(".NET")); // E filtraria isso em memória
+
+                var tags2 = context // Chegaria no contexto
+                    .Tags         // Chegaria nas tags
+                    .Where(x => x.Name.Contains(".NET")) // Filtra primeiro
+                    .ToList(); // ToList() sempre por último
+
+                // O ToList() força a execução da Query
+                //var tags = context.Tags.ToList();
+
+                foreach (var tag in tags)
+                {
+                    Console.WriteLine($"{tag.Name} - {tag.Slug}");
+                }
             }
         }
     }
