bc14ed1997906809b37ea5da67b65371da5346bb
Gabriel <santos.tgabriel@hotmail.com>
2022-08-16 08:09:27 -0300

    DataAnnotation

--- Fundamentos do Entity Framework/Módulo 2/Blog/Models/Category.cs
+++ Fundamentos do Entity Framework/Módulo 2/Blog/Models/Category.cs
@@ -1,7 +1,15 @@
-﻿namespace Blog.Models
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Blog.Models
 {
+    [Table("Category")]
     public class Category
     {
+        [Key]
+        // O banco que gera esse Id usando Identity(1 ,1)
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Slug { get; set; }
