d73b62afd4f606272b8a87149aa2f1d60fcc7076
Gabriel <santos.tgabriel@hotmail.com>
2022-08-16 08:58:45 -0300

    Trabalhando com Sub Conjuntos

--- Fundamentos do Entity Framework/Módulo 2/Blog/Program.cs
+++ Fundamentos do Entity Framework/Módulo 2/Blog/Program.cs
@@ -9,10 +9,38 @@ namespace Blog
     {
         static void Main(string[] args)
         {
-            using(var context = new BlogDataContext())
+            using var context = new BlogDataContext();
+
+            var user = new User
             {
-                
-            }
+                Name = "André Baltieri",
+                Slug = "andrebaltieri",
+                Email = "andre@balta.io",
+                Bio = "9x Microsoft MVP",
+                Image = "https://balta.io",
+                PasswordHash = "123098457"
+            };
+
+            var category = new Category
+            {
+                Name = "Backend",
+                Slug = "backend"
+            };
+
+            var post = new Post
+            {
+                Author = user,
+                Category = category,
+                Body = "<p>Hello World</p>",
+                Slug = "comecando-com-ef-core",
+                Summary = "Neste artigo vamos aprender EF Core",
+                Title = "Comecando com EF core",
+                CreateDate = DateTime.Now,
+                LastUpdateDate = DateTime.Now
+            };
+
+            context.Posts.Add(post);
+            context.SaveChanges();
         }
     }
 }
