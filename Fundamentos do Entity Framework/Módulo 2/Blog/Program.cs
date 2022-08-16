fa41e58178debd547c758f7bea065ae2a5a09701
Gabriel <santos.tgabriel@hotmail.com>
2022-08-16 19:15:10 -0300

    Alterando um subconjunto

--- Fundamentos do Entity Framework/Módulo 2/Blog/Program.cs
+++ Fundamentos do Entity Framework/Módulo 2/Blog/Program.cs
@@ -11,56 +11,18 @@ namespace Blog
         {
             using var context = new BlogDataContext();
 
-            //var user = new User
-            //{
-            //    Name = "André Baltieri",
-            //    Slug = "andrebaltieri",
-            //    Email = "andre@balta.io",
-            //    Bio = "9x Microsoft MVP",
-            //    Image = "https://balta.io",
-            //    PasswordHash = "123098457"
-            //};
-
-            //var category = new Category
-            //{
-            //    Name = "Backend",
-            //    Slug = "backend"
-            //};
-
-            //var post = new Post
-            //{
-            //    Author = user,
-            //    Category = category,
-            //    Body = "<p>Hello World</p>",
-            //    Slug = "comecando-com-ef-core",
-            //    Summary = "Neste artigo vamos aprender EF Core",
-            //    Title = "Comecando com EF core",
-            //    CreateDate = DateTime.Now,
-            //    LastUpdateDate = DateTime.Now
-            //};
-
-            //context.Posts.Add(post);
-            //context.SaveChanges();
-
-            //var posts = context
-            //    .Posts
-            //    .AsNoTracking()
-            //    //.Where(x => x.Author.Id == 1005) para fazer isso precisaria dar um JOIN
-            //    //.Where(x => x.AuthorId == 1005)
-            //    .OrderByDescending(x => x.LastUpdateDate)
-            //    .ToList();
-
-            var posts = context
+            var post = context
                 .Posts
-                .AsNoTracking()
-                .Include(x => x.Author) // propriedade do post
+                //.AsNoTracking() // PRECISA DO TRACKING
+                .Include(x => x.Author)
                 .Include(x => x.Category)
-                //.ThenInclude() pega item do filho (propriedade da Category) SUBSELECT
                 .OrderByDescending(x => x.LastUpdateDate)
-                .ToList();
+                .FirstOrDefault();
 
-            foreach (var post in posts)
-                Console.WriteLine($"{post.Title} escrito por {post.Author?.Name} em {post.Category?.Name}");
+            post.Author.Name = "Teste"; // Mudou no banco
+
+            context.Update(post);
+            context.SaveChanges();
         }
     }
 }
