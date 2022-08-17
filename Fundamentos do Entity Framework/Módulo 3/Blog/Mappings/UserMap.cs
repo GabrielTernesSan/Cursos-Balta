43120db308c871ed3ca20cc76f2f78e4a4800978
Gabriel <santos.tgabriel@hotmail.com>
2022-08-17 10:39:05 -0300

    Iniciando o mapeamento do user

---
+++ Fundamentos do Entity Framework/Módulo 3/Blog/Mappings/UserMap.cs
@@ -0,0 +1,50 @@
+﻿using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Mappings
+{
+    public class UserMap : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            // Tabela
+            builder.ToTable("User");
+
+            // Chave primária
+            builder.HasKey(x => x.Id);
+
+            // Identity
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn();
+
+            // Propriedades
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasColumnName("Name")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(80);
+
+            builder.Property(x => x.Bio);
+            builder.Property(x => x.Email);
+            builder.Property(x => x.Image);
+            builder.Property(x => x.PasswordHash);
+
+            builder.Property(x => x.Slug)
+                .IsRequired()
+                .HasColumnName("Slug")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(80);
+
+            // Índice
+            builder.HasIndex(x => x.Slug, "IX_User_Slug")
+                .IsUnique();
+        }
+    }
+}
