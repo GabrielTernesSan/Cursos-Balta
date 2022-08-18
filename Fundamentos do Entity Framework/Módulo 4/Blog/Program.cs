df19f0d7761cbb24ba4205a4ba571c11cfab8bb7
Gabriel <santos.tgabriel@hotmail.com>
2022-08-18 10:16:46 -0300

    Gerando nova versao do banco

--- Fundamentos do Entity Framework/Módulo 4/Blog/Program.cs
+++ Fundamentos do Entity Framework/Módulo 4/Blog/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace Blog
+﻿using Blog.Data;
+using Blog.Models;
+
+namespace Blog
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Update-database para criar/atulizar o banco      
+            // Vai dar erro, porque foi introduzido campo 'GitHub' e no banco não
+            using var context = new BlogDataContext();
+
+            context.Users.Add(new User()
+            {
+                Bio = "9x Microsoft MVP",
+                GitHub = "GabrielTernesSan",
+                Email = "andre@balta.io",
+                Image = "https://balta.io",
+                Name = "André Baltieri",
+                PasswordHash = "hash",
+                Slug = "andre-baltieri"
+            });
+            context.SaveChanges();
+            // para atualizar, cria uma nova migration
+            // update-database depois
         }
     }
 }
