99531b5bf423cf767db469bcbe2ad16a367c3769
Gabriel <santos.tgabriel@hotmail.com>
2022-08-18 10:22:36 -0300

    Gerando script do banco

--- Fundamentos do Entity Framework/Módulo 4/Blog/Program.cs
+++ Fundamentos do Entity Framework/Módulo 4/Blog/Program.cs
@@ -7,22 +7,7 @@ namespace Blog
     {
         static void Main(string[] args)
         {
-            // Vai dar erro, porque foi introduzido campo 'GitHub' e no banco não
-            using var context = new BlogDataContext();
 
-            context.Users.Add(new User()
-            {
-                Bio = "9x Microsoft MVP",
-                GitHub = "GabrielTernesSan",
-                Email = "andre@balta.io",
-                Image = "https://balta.io",
-                Name = "André Baltieri",
-                PasswordHash = "hash",
-                Slug = "andre-baltieri"
-            });
-            context.SaveChanges();
-            // para atualizar, cria uma nova migration
-            // update-database depois
         }
     }
 }
