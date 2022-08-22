ef0136be94820805ec62d3c207ffa1abd0efbd6b
Gabriel <santos.tgabriel@hotmail.com>
2022-08-22 13:37:11 -0300

    Iniciando os Controllers

---
+++ Fundamentos do ASP.NET 6/Módulo 3/Blog/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        // Health Check
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                status = "Online"
+            });
+        }
+    }
+}
