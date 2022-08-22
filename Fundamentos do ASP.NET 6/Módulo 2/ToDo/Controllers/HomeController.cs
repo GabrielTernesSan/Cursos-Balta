6dc79f4ff0a8bc233407a18aa766d5d5ce19b67f
Gabriel <santos.tgabriel@hotmail.com>
2022-08-22 10:42:10 -0300

    Atualizando e excluindo um registro

--- Fundamentos do ASP.NET 6/Módulo 2/ToDo/Controllers/HomeController.cs
+++ Fundamentos do ASP.NET 6/Módulo 2/ToDo/Controllers/HomeController.cs
@@ -18,10 +18,44 @@ namespace ToDo.Controllers
         [HttpGet]
         public List<ToDoModel> Get() => _appDbContext.ToDos.ToList();
 
+        [HttpGet("{id:int}")]
+        public ToDoModel GetById([FromRoute] int id)
+        {
+            return _appDbContext.ToDos.FirstOrDefault(x => x.Id == id);
+        }
+
         [HttpPost]
         public ToDoModel Post([FromBody] ToDoModel model)
         {
             _appDbContext.ToDos.Add(model);
+            _appDbContext.SaveChanges();
+            return model;
+        }
+
+        [HttpPut("{id:int}")]
+        public ToDoModel Put([FromRoute] int id, [FromBody] ToDoModel ToDo)
+        {
+            var toDo = _appDbContext.ToDos.FirstOrDefault(x => x.Id == id);
+
+            if (toDo == null)
+            {
+                return toDo;
+            }
+
+            toDo.Title = ToDo.Title;
+            toDo.Done = ToDo.Done;
+
+            _appDbContext.ToDos.Update(toDo);
+            _appDbContext.SaveChanges();
+
+            return toDo;
+        }
+
+        [HttpDelete("{id:int}")]
+        public ToDoModel Delete([FromRoute] int id)
+        {
+            var model = _appDbContext.ToDos.FirstOrDefault(x => x.Id == id);
+            _appDbContext.ToDos.Remove(model);
             _appDbContext.SaveChanges();
             return model;
         }
