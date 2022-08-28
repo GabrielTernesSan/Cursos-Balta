59bbb3edb265835211a351dcadd1f9d147f49beb
Gabriel <santos.tgabriel@hotmail.com>
2022-08-28 10:08:15 -0300

    CRUD de Categorias

--- Fundamentos do ASP.NET 6/Módulo 3/Blog/Controllers/CategoryController.cs
+++ Fundamentos do ASP.NET 6/Módulo 3/Blog/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Blog.Data;
+using Blog.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -20,5 +21,81 @@ namespace Blog.Controllers
             var categories = await _context.Categories.ToListAsync();
             return Ok(categories);
         }
+
+        [HttpGet("v1/categories/{id:int}")]
+        public async Task<IActionResult> GetByIdAsync([FromRoute] int id)
+        {
+            try
+            {
+                var category = await _context
+                    .Categories
+                    .FirstOrDefaultAsync(x => x.Id == id);
+
+                return Ok(category);
+            }catch(Exception ex)
+            {
+                return NotFound($"Id não encontrado! \n {ex.Message}");
+            }
+        }
+
+        [HttpPost("v1/categories")]
+        public async Task<IActionResult> PostAsync([FromBody] Category model)
+        {
+            try
+            {
+                await _context.Categories.AddAsync(model);
+                await _context.SaveChangesAsync();
+
+                return Created($"v1/categories/{model.Id}", model);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Verifique os dados e tente novamente! \n {ex.Message}");
+            }
+        }
+
+        [HttpPut("v1/categories/{int:id}")]
+        public async Task<IActionResult> PutAsync([FromRoute] int id, [FromBody] Category model)
+        {
+            try
+            {
+                var category = await _context
+                    .Categories
+                    .FirstOrDefaultAsync(x => x.Id == id);
+
+                category.Name = model.Name;
+                category.Slug = model.Slug;
+
+                _context.Categories.Update(category);
+                await _context.SaveChangesAsync();
+
+                return Ok(model);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Verifique os dados e tente novamente! \n {ex.Message}");
+            }
+        }
+
+        [HttpDelete("v1/categories/{id:int}")]
+        public async Task<IActionResult> DeleteAsync([FromRoute] int id)
+        {
+            try
+            {
+                var category = await _context
+                    .Categories
+                    .FirstOrDefaultAsync(x => x.Id == id);
+
+                _context.Categories.Remove(category);
+                await _context.SaveChangesAsync();
+
+                return Ok(category);
+            }
+            catch (Exception ex)
+            {
+                return NotFound($"Id não encontrado! \n {ex.Message}");
+            }
+        }
+
     }
 }
