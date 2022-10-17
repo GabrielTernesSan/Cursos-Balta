8d6ea6b4a4dec5f4091241139697fe503e69109d
Gabriel <santos.tgabriel@hotmail.com>
2022-10-17 19:42:26 -0300

    PromoCode Repository

--- Dominando Injeção de Dependência/Demos/01 - Inicial/DependencyStore/Controllers/OrderController.cs
+++ Dominando Injeção de Dependência/Demos/01 - Inicial/DependencyStore/Controllers/OrderController.cs
@@ -13,24 +13,27 @@ public class OrderController : ControllerBase
 
     private readonly ICustomerRepository _customerRepository;
     private readonly IDeliveryFeeService _deliveryFeeService;
+    private readonly IPromoCodeRepository _promoCodeRepository;
 
-    public OrderController(ICustomerRepository customerRepository, IDeliveryFeeService deliveryFeeService)
+    public OrderController(ICustomerRepository customerRepository, IDeliveryFeeService deliveryFeeService,
+    IPromoCodeRepository promoCodeRepository)
     {
         _customerRepository = customerRepository;
         _deliveryFeeService = deliveryFeeService;
+        _promoCodeRepository = promoCodeRepository;
     }
 
     [Route("v1/orders")]
     [HttpPost]
     public async Task<IActionResult> Place(string customerId, string zipCode, string promoCode, int[] products)
     {
-        // #1 - Recupera o cliente
         var customer = await _customerRepository.GetByIdAsync(customerId);
         if (customer == null)
             return BadRequest();
 
-        // #2 - Calcula o frete
         decimal deliveryFee = await _deliveryFeeService.GetDeliveryFeeAsync(zipCode);
+
+        var cupom = await _promoCodeRepository.GetPromoCodeAsync(promoCode);
 
         // #3 - Calcula o total dos produtos
         decimal subTotal = 0;
