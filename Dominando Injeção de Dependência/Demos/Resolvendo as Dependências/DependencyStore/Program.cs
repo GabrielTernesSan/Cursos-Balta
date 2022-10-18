3df3059891663422b71e1e1ebf341dba1cd1baf6
Gabriel <santos.tgabriel@hotmail.com>
2022-10-18 11:28:23 -0300

    Implementando Extension Methods

--- Dominando Injeção de Dependência/Demos/Resolvendo as Dependências/DependencyStore/Program.cs
+++ Dominando Injeção de Dependência/Demos/Resolvendo as Dependências/DependencyStore/Program.cs
@@ -1,29 +1,15 @@
-using DependencyStore.Repositories.Contracts;
-using DependencyStore.Repositories;
-using DependencyStore.Services.Contracts;
-using DependencyStore.Services;
-using Microsoft.Data.SqlClient;
 using DependencyStore;
+using DependencyStore.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// O AddSingleton instância uma depenência uma única vez ao rodar a aplicação
-// geralmente usado para definir configurações do sistema
 builder.Services.AddSingleton<Configuration>();
 
-/* 
-    Preciso resolver o SqlConnection também, porém se usar Transient toda vez que passar pelo construtor ele cria 
-    uma nova conexão. O ideal é criarmos a menor quantidade de conexões possíveis, por isso, usamos o AddScoped
-    que cria uma conexão por requisição
-*/
-builder.Services.AddScoped<SqlConnection>(x
-    => new SqlConnection("CONN_STRING")
-);
+var connStr = builder.Configuration.GetConnectionString("DefaultConnection");
 
-// Transient -> Toda vez que passar pelo contrutor será feita uma nova instância
-builder.Services.AddTransient<ICustomerRepository, CustomerRepository>();
-builder.Services.AddTransient<IPromoCodeRepository, PromoCodeRepository>();
-builder.Services.AddTransient<IDeliveryFeeService, DeliveryFeeService>();
+builder.Services.AddSqlConnection(connStr);
+builder.Services.AddRepositories();
+builder.Services.AddServices();
 
 builder.Services.AddControllers();
 
