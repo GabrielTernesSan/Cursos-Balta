793196135f9586fbdf3a22427495ab1b8f40ade1
Gabriel <santos.tgabriel@hotmail.com>
2022-10-18 08:47:00 -0300

    Resolvendo dependências - AddScoped

--- Dominando Injeção de Dependência/Demos/Resolvendo as Dependências/DependencyStore/Program.cs
+++ Dominando Injeção de Dependência/Demos/Resolvendo as Dependências/DependencyStore/Program.cs
@@ -2,8 +2,19 @@ using DependencyStore.Repositories.Contracts;
 using DependencyStore.Repositories;
 using DependencyStore.Services.Contracts;
 using DependencyStore.Services;
+using Microsoft.Data.SqlClient;
 
 var builder = WebApplication.CreateBuilder(args);
+
+/* 
+    Preciso resolver o SqlConnection também, porém se usar Transient toda vez que passar pelo construtor ele cria 
+    uma nova conexão. O ideal é criarmos a menor quantidade de conexões possíveis, por isso, usamos o AddScoped
+    que cria uma conexão por requisição
+*/
+
+builder.Services.AddScoped<SqlConnection>(x
+    => new SqlConnection("CONN_STRING")
+);
 
 // Transient -> Toda vez que passar pelo contrutor será feita uma nova instância
 builder.Services.AddTransient<ICustomerRepository, CustomerRepository>();
