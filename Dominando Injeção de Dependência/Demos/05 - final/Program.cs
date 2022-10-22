64b70ed005b79ae4288cd1368c065114b2ae9a4e
Gabriel <santos.tgabriel@hotmail.com>
2022-10-22 11:03:00 -0300

    GetRequiredServices

--- Dominando Injeção de Dependência/Demos/05 - final/Program.cs
+++ Dominando Injeção de Dependência/Demos/05 - final/Program.cs
@@ -2,6 +2,7 @@ using ResolvingDependencies.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Sempre uma nova instância
 builder.Services.AddTransient<IWeatherService, WeatherService>();
 builder.Services.AddControllers();
 
@@ -9,11 +10,13 @@ var app = builder.Build();
 
 app.MapControllers();
 
+// Resolvendo dependências no Program
 using (var scope = app.Services.CreateScope())
 {
     var service = scope
         .ServiceProvider
         .GetRequiredService<IWeatherService>();
+
     service.Get();
 }
 
