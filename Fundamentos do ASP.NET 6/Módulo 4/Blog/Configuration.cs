99448d894fd2f7d9d4ff1989ebe0f3efbde1f2cb
Gabriel <santos.tgabriel@hotmail.com>
2022-10-24 10:31:59 -0300

    Token e JWT

---
+++ Fundamentos do ASP.NET 6/Módulo 4/Blog/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace Blog
+{
+    public static class Configuration
+    {
+        // TOKEN - JWT - Json Web Token
+        public static string JwtKey { get; set; } = "Eh6zE1b1MUyioEhAKQFlEQ==";
+    }
+}
