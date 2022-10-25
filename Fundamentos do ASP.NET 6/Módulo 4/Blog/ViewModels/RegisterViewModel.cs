0c13ee951862eb162d905ce124f00e78aa7f2408
Gabriel <santos.tgabriel@hotmail.com>
2022-10-25 09:41:24 -0300

    Iniciando o cadastro de usuários

---
+++ Fundamentos do ASP.NET 6/Módulo 4/Blog/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O e-mail é obrigatório")]
+        [EmailAddress(ErrorMessage = "E-mail inválido")]
+        public string Email { get; set; }
+    }
+}
