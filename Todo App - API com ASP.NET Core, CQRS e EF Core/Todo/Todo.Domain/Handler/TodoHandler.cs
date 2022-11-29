1a1bd673ec690a9a79c96d582809c3f3cfd3fd0c
Gabriel <santos.tgabriel@hotmail.com>
2022-11-29 13:32:43 -0300

    Começando o Handler

---
+++ Todo App - API com ASP.NET Core, CQRS e EF Core/Todo/Todo.Domain/Handler/TodoHandler.cs
@@ -0,0 +1,24 @@
+﻿using Flunt.Notifications;
+using Todo.Domain.Commands;
+using Todo.Domain.Commands.Contracts;
+using Todo.Domain.Repositories;
+using static Todo.Domain.Handler.Contracts.IHandler;
+
+namespace Todo.Domain.Handler
+{
+    public class TodoHandler : Notifiable, IHandler<CreateTodoCommand>
+    {
+
+        private readonly ITodoRepository _repository;
+
+        public TodoHandler(ITodoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ICommandResult Handle(CreateTodoCommand command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
