34ce1a6b76385bf283fef5fc9da178bac5216a89
Gabriel <santos.tgabriel@hotmail.com>
2022-11-30 11:21:34 -0300

    Iniciando os testes dos handlers

---
+++ Todo App - API com ASP.NET Core, CQRS e EF Core/Todo/Todo.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
@@ -0,0 +1,18 @@
+﻿namespace Todo.Domain.Tests.HandlerTests
+{
+    [TestClass]
+    public class CreateTodoHandlerTests
+    {
+        [TestMethod]
+        public void Dado_um_comando_invalido_deve_interromper_a_execucao()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Dado_um_comando_valido_deve_criar_a_tarefa()
+        {
+            Assert.Fail();
+        }
+    }
+}
