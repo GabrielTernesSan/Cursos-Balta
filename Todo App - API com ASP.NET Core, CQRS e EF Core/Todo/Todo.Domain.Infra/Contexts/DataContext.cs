a5ce4486aaf97ee16aa981d9f4d600aa5e688dfd
Gabriel <santos.tgabriel@hotmail.com>
2022-12-01 10:42:24 -0300

    Alterando os mapeamentos

--- Todo App - API com ASP.NET Core, CQRS e EF Core/Todo/Todo.Domain.Infra/Contexts/DataContext.cs
+++ Todo App - API com ASP.NET Core, CQRS e EF Core/Todo/Todo.Domain.Infra/Contexts/DataContext.cs
@@ -8,5 +8,16 @@ namespace Todo.Domain.Infra.Contexts
         public DataContext(DbContextOptions<DataContext> options) : base(options){ }
 
         public DbSet<TodoItem> Todos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TodoItem>().ToTable("Todo");
+            modelBuilder.Entity<TodoItem>().Property(s => s.Id);
+            modelBuilder.Entity<TodoItem>().Property(s => s.User).HasMaxLength(120).HasColumnType("varchar");
+            modelBuilder.Entity<TodoItem>().Property(s => s.Title).HasMaxLength(160).HasColumnType("varchar(160)");
+            modelBuilder.Entity<TodoItem>().Property(s => s.Done).HasColumnType("bit");
+            modelBuilder.Entity<TodoItem>().Property(s => s.Date);
+            modelBuilder.Entity<TodoItem>().HasIndex(b => b.User);
+        }
     }
 }
