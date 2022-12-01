a374aa6c8ddd403e052cdd49eb67cbc9ce75395f
Gabriel <santos.tgabriel@hotmail.com>
2022-12-01 08:27:59 -0300

    Finalizando o Handler

--- Todo App - API com ASP.NET Core, CQRS e EF Core/Todo/Todo.Domain/Handler/TodoHandler.cs
+++ Todo App - API com ASP.NET Core, CQRS e EF Core/Todo/Todo.Domain/Handler/TodoHandler.cs
@@ -7,7 +7,7 @@ using static Todo.Domain.Handler.Contracts.IHandler;
 
 namespace Todo.Domain.Handler
 {
-    public class TodoHandler : Notifiable, IHandler<CreateTodoCommand>, IHandler<UpdateTodoCommand>
+    public class TodoHandler : Notifiable, IHandler<CreateTodoCommand>, IHandler<UpdateTodoCommand>, IHandler<MarkTodoAsDoneCommand>, IHandler<MarkTodoAsUndoneCommand>
     {
 
         private readonly ITodoRepository _repository;
@@ -59,6 +59,56 @@ namespace Todo.Domain.Handler
             // Salva no banco
             _repository.Update(todo);
             
+            // Retorna o resultado
+            return new GenericCommandResult(true, "Tarefa salva", todo);
+        }
+
+        public ICommandResult Handle(MarkTodoAsDoneCommand command)
+        {
+            // Fail Fast Validation
+            command.Validate();
+            if (command.Invalid)
+            {
+                return new GenericCommandResult(
+                    false,
+                    "Ops, parece que sua tarefa está errada",
+                    command.Notifications);
+            }
+
+            // Recupera o TodoItem (Reidratação)
+            var todo = _repository.GetById(command.Id, command.User);
+
+            // Altera o estado
+            todo.MarkAsDone();
+
+            // Salva no banco
+            _repository.Update(todo);
+
+            // Retorna o resultado
+            return new GenericCommandResult(true, "Tarefa salva", todo);
+        }
+
+        public ICommandResult Handle(MarkTodoAsUndoneCommand command)
+        {
+            // Fail Fast Validation
+            command.Validate();
+            if (command.Invalid)
+            {
+                return new GenericCommandResult(
+                    false,
+                    "Ops, parece que sua tarefa está errada",
+                    command.Notifications);
+            }
+
+            // Recupera o TodoItem (Reidratação)
+            var todo = _repository.GetById(command.Id, command.User);
+
+            // Altera o estado
+            todo.MarkAsUndone();
+
+            // Salva no banco
+            _repository.Update(todo);
+
             // Retorna o resultado
             return new GenericCommandResult(true, "Tarefa salva", todo);
         }
