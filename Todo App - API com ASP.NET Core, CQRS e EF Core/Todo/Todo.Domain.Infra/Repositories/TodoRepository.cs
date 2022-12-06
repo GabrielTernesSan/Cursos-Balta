6c121d1f312915dc76fe7a5d69091c212468e77a
Gabriel <santos.tgabriel@hotmail.com>
2022-12-06 09:54:29 -0300

    Criando e Atualizando uma tarefa

--- Todo App - API com ASP.NET Core, CQRS e EF Core/Todo/Todo.Domain.Infra/Repositories/TodoRepository.cs
+++ Todo App - API com ASP.NET Core, CQRS e EF Core/Todo/Todo.Domain.Infra/Repositories/TodoRepository.cs
@@ -1,13 +1,24 @@
-﻿using Todo.Domain.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using Todo.Domain.Entities;
+using Todo.Domain.Infra.Contexts;
 using Todo.Domain.Repositories;
 
 namespace Todo.Domain.Infra.Repositories
 {
     public class TodoRepository : ITodoRepository
     {
+
+        private readonly DataContext _context;
+
+        public TodoRepository(DataContext context)
+        {
+            _context = context;
+        }
+
         public void Create(TodoItem todo)
         {
-            throw new NotImplementedException();
+            _context.Todos.Add(todo);
+            _context.SaveChanges();
         }
 
         public TodoItem GetById(Guid id, string user)
@@ -17,7 +28,8 @@ namespace Todo.Domain.Infra.Repositories
 
         public void Update(TodoItem todo)
         {
-            throw new NotImplementedException();
+            _context.Entry(todo).State = EntityState.Modified;
+            _context.SaveChanges();
         }
 
         public IEnumerable<TodoItem> GetAll(string user)
