24e09fe604a8675e49db054992bc3560f0fe2aaa
Gabriel <santos.tgabriel@hotmail.com>
2022-12-06 11:29:32 -0300

    Criando uma tarefa

---
+++ Todo App - API com ASP.NET Core, CQRS e EF Core/Todo/Todo.Domain.Api/Controllers/TodoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Todo.Domain.Commands;
+using Todo.Domain.Handler;
+
+namespace Todo.Domain.Api.Controllers
+{
+    [ApiController]
+    [Route("v1/todos")]
+    public class TodoController : Controller
+    {
+        [HttpPost]
+        public GenericCommandResult Create([FromBody] CreateTodoCommand command, [FromServices] TodoHandler handler)
+        {
+            command.User = "GabrielTernes";
+            return (GenericCommandResult)handler.Handle(command);
+        }
+    }
+}
