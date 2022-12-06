fb4c5dc5d6379e9a0275d48b736185210f47c1ce
Gabriel <santos.tgabriel@hotmail.com>
2022-12-06 10:36:31 -0300

    Executando uma query

--- Todo App - API com ASP.NET Core, CQRS e EF Core/Todo/Todo.Domain.Infra/Repositories/TodoRepository.cs
+++ Todo App - API com ASP.NET Core, CQRS e EF Core/Todo/Todo.Domain.Infra/Repositories/TodoRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Todo.Domain.Entities;
 using Todo.Domain.Infra.Contexts;
+using Todo.Domain.Queries;
 using Todo.Domain.Repositories;
 
 namespace Todo.Domain.Infra.Repositories
@@ -34,7 +35,7 @@ namespace Todo.Domain.Infra.Repositories
 
         public IEnumerable<TodoItem> GetAll(string user)
         {
-            throw new NotImplementedException();
+            return _context.Todos.AsNoTracking().Where(ToDoQueries.GetAll(user)).OrderBy(x => x.Date);
         }
 
         public IEnumerable<TodoItem> GetAllDone(string user)
