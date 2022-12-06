9ef82d82f1af3457e8f211c7c2db7a50b70d32b9
Gabriel <santos.tgabriel@hotmail.com>
2022-12-06 08:40:25 -0300

    Resolvendo dependências e Utilizando Banco de Dados em Memória

--- Todo App - API com ASP.NET Core, CQRS e EF Core/Todo/Todo.Domain.Api/Program.cs
+++ Todo App - API com ASP.NET Core, CQRS e EF Core/Todo/Todo.Domain.Api/Program.cs
@@ -1,15 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.InteropServices;
+using Todo.Domain.Infra.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<DataContext>(optional => optional.UseInMemoryDatabase("Database"));
+//builder.Services.AddDbContext<DataContext>(optional => optional.UseSqlServer(Configuration.GetConnectionString("connectionString"));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
