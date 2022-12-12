b36b7e23709f689c4846ed4ce10991cdc21bbe09
Gabriel <santos.tgabriel@hotmail.com>
2022-12-12 19:20:30 -0300

    Adicionando autenticação pelo Google

--- Todo App - API com ASP.NET Core, CQRS e EF Core/Todo/Todo.Domain.Api/Program.cs
+++ Todo App - API com ASP.NET Core, CQRS e EF Core/Todo/Todo.Domain.Api/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using Todo.Domain.Handler;
 using Todo.Domain.Infra.Contexts;
 using Todo.Domain.Infra.Repositories;
@@ -16,6 +19,19 @@ builder.Services.AddDbContext<DataContext>(optional => optional.UseSqlServer(bui
 
 builder.Services.AddTransient<ITodoRepository, TodoRepository>();
 builder.Services.AddTransient<TodoHandler, TodoHandler>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.Authority = "https://securetoken.google.com/todo-app-d71c8";
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = "https://securetoken.google.com/todo-app-d71c8",
+        ValidateAudience = true,
+        ValidAudience = "todo-app-d71c8",
+        ValidateLifetime = true
+    };
+});
 
 var app = builder.Build();
 
