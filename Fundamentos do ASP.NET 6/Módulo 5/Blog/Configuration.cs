84e100fe5b8e62cee0298cad2538f0425ad0ee90
Gabriel <santos.tgabriel@hotmail.com>
2022-12-15 08:00:50 -0300

    Módulo 5

---
+++ Fundamentos do ASP.NET 6/Módulo 5/Blog/Configuration.cs
@@ -0,0 +1,10 @@
+﻿namespace Blog
+{
+    public static class Configuration
+    {
+        // TOKEN - JWT - Json Web Token
+        public static string JwtKey = "Eh6zE1b1MUyioEhAKQFlEQ==";
+        public static string ApiKeyName = "api_key";
+        public static string ApiKey = "curso_api_IlTevUM/z0ey3NwCV/unWg==";
+    }
+}
---
+++ Fundamentos do ASP.NET 6/Módulo 5/Blog/Program.cs
@@ -0,0 +1,55 @@
+using Blog;
+using Blog.Data;
+using Blog.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true, // Validar chave de assinatura? true
+        IssuerSigningKey = new SymmetricSecurityKey(key), // Como validar? Nova chave simétrica
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
+builder.Services
+    .AddControllers()
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        options.SuppressModelStateInvalidFilter = true;
+    });
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
+
+builder.Services.AddDbContext<BlogDataContext>();
+builder.Services.AddTransient<TokenService>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
