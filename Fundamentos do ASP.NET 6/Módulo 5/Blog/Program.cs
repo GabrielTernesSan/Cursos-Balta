59787f2ccca82c9a03f01d5f36230989afc6f2eb
Gabriel <santos.tgabriel@hotmail.com>
2022-12-16 08:24:22 -0300

    Arquivos estáticos

--- Fundamentos do ASP.NET 6/Módulo 5/Blog/Program.cs
+++ Fundamentos do ASP.NET 6/Módulo 5/Blog/Program.cs
@@ -35,7 +35,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
 app.UseAuthentication();
 app.UseAuthorization();
 
--- Fundamentos do ASP.NET 6/Módulo 5/Blog/ViewModels/Accounts/LoginViewModel.cs
+++ Fundamentos do ASP.NET 6/Módulo 5/Blog/ViewModels/Accounts/LoginViewModel.cs
@@ -1,14 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace Blog.ViewModels
+namespace Blog.ViewModels.Accounts
 {
-    public class RegisterViewModel
+    public class LoginViewModel
     {
-        [Required(ErrorMessage = "O nome é obrigatório")]
-        public string Nome { get; set; }
-
         [Required(ErrorMessage = "O e-mail é obrigatório")]
         [EmailAddress(ErrorMessage = "E-mail inválido")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Informe a senha")]
+        public string Password { get; set; }
     }
 }
