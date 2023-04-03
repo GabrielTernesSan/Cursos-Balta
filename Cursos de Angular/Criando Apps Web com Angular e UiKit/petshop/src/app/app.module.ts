6ec9376e2fc5a2034c31cab95eb2c023d25a6469
GabrielTernesSan <santos.tgabriel@hotmail.com>
2023-04-03 10:46:12 -0300

    Configurando rotas

--- Cursos de Angular/Criando Apps Web com Angular e UiKit/petshop/src/app/app.module.ts
+++ Cursos de Angular/Criando Apps Web com Angular e UiKit/petshop/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { SignupPageComponent } from './pages/accounts/signup-page/signup-page.co
 import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
 import { PetsPageComponent } from './pages/accounts/pets-page/pets-page.component';
 import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
+import { FramePageComponent } from './pages/master/frame.page';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
     SignupPageComponent,
     ProductsPageComponent,
     PetsPageComponent,
-    CartPageComponent
+    CartPageComponent,
+    FramePageComponent
   ],
   imports: [
     BrowserModule,
